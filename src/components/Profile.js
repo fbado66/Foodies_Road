a5ccfe5cada3f7585238a0421b463940392cd9ea
fbado66 <fbado66@gmail.com>
2020-11-10 14:12:29 -0500

    working on logic to render pass orders in profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import UpdateUserForm from './UpdateUserForm'
+// import UpdateUserForm from './UpdateUserForm'
 import {withRouter, Redirect } from 'react-router-dom'
-import { ThreeSixtySharp } from '@material-ui/icons'
 
 class Profile extends React.Component {
 
@@ -26,7 +25,8 @@ class Profile extends React.Component {
   
     passTransactions = this.props.cart.map(transactions => {
         return transactions.orders.map(productPojo => {
-            return <div>Transaction ID:{transactions.id}
+            return <div key={productPojo.id}>
+                        Transaction ID:{transactions.id}
                         <div id={productPojo.product} >
                             <p>name: {productPojo.product.name}</p>
                         </div>
@@ -34,11 +34,58 @@ class Profile extends React.Component {
         })
     })
 
+
+
+    test = this.props.cart.reduce((acc, {id, orders}) => {
+        if(!acc[id]) acc[id] = [];
+        acc[id].push(orders)
+        return acc
+    }, {})
+
+    // test = this.props.cart.map(transactions => {
+    //     transactions.reduce((acc, {id, orders}) => {
+    //             if(!acc[id]) acc[id] = [];
+    //             acc[id].push(orders)
+    //             return acc
+    //         }, {})
+    // })
+    test2 = Object.values(this.test).map( orderPOjo => {
+        return orderPOjo.map(productPojo => {
+            return productPojo.map(order => {
+                return <p>{order.cart_id}
+                <div>ORDER : {order.product.name}
+                </div>
+                </p>
+            })
+        })
+    })
+    
+    test3 = this.props.cart.map(cartpojo => {
+        return cartpojo.orders.map(orderpojo => {
+            return <div>
+                {cartpojo.id}
+                <p>{orderpojo.product.name}</p>
+            </div>
+        })
+    })
+    
+    
+
     render() {
 
-        console.log(this.props.cart.map(cartPojo => {
-            console.log(cartPojo.id)
-        }))
+        console.log(this.test2)
+        console.log(this.test)
+        console.log(this.props.cart)
+        // console.log(this.props.cart.map(cartpojo => {
+        //     return cartpojo.orders.map(orderpojo => {
+        //         return <div>{orderpojo.product.name}
+        //         {cartpojo.id}</div>
+        //     })
+        // }))
+
+        // console.log(this.props.cart.map(cartPojo => {
+        //     console.log(cartPojo.id)
+        // }))
         return (
             <div>
                 <h2>Welcome {this.props.name}</h2>
@@ -52,7 +99,8 @@ class Profile extends React.Component {
 
                 <div>
                     <p>previous Transactions</p>
-                    {this.passTransactions}
+                    {/* {this.passTransactions} */}
+                    {this.test2}
                     <p>{this.props.transaction.amount}</p>
                 </div>
 
